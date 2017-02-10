5b5cb53bd85e61b54d4586b5f2d85be29c91747b
egibar <aeguibar006@ikasle.ehu.eus>
2017-02-10 13:11:51 +0100

    base de datos creada

---
+++ Librerias/BD.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Data.SqlClient
+Public Class BD
+    Private Shared conexion As New SqlConnection
+    Private Shared comando As New SqlCommand
+    Public Shared Function conectar() As String
+        Try
+            conexion.ConnectionString = "Data Source=158.227.106.20;Initial_
+Catalog=Amigos;Integrated_
+Security=False;_UserID=HADS;Password=********;_
+Connect Timeout=15;Encrypt=False;TrustServerCertificate=False"
+            conexion.Open()
+        Catch ex As Exception
+            Return "ERROR DE CONEXIÓN: " + ex.Message
+        End Try
+        Return "CONEXION OK"
+    End Function
+End Class
---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Usuarios] (
+    [email]      NVARCHAR (50) NOT NULL,
+    [nombre]     NVARCHAR (50) NULL,
+    [apellidos]  NVARCHAR (70) NULL,
+    [pregunta]   NVARCHAR (50) NULL,
+    [respuesta]  NVARCHAR (50) NULL,
+    [dni]        NVARCHAR (15) NULL,
+    [numconfir]  INT           NULL,
+    [confirmado] BIT           NULL,
+    [grupo]      NVARCHAR (50) NULL,
+    [tipo]       NVARCHAR (50) NULL,
+    [pass]       NVARCHAR (50) NULL,
+    CONSTRAINT [PK__Usuarios__0000000000000095] PRIMARY KEY CLUSTERED ([email] ASC)
+);
